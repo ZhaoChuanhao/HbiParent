54dda6e45d3259e82d7306106a5920481c544b4d
赵传昊 <724356121@qq.com>
2018-08-18 14:49:01 +0800

    临时更新

--- core/src/main/java/hbi/demo/controllers/OmOrderHeadersController.java
+++ core/src/main/java/hbi/demo/controllers/OmOrderHeadersController.java
@@ -1,20 +1,19 @@
 package hbi.demo.controllers;
 
-import org.springframework.stereotype.Controller;
-import com.hand.hap.system.controllers.BaseController;
 import com.hand.hap.core.IRequest;
+import com.hand.hap.system.controllers.BaseController;
 import com.hand.hap.system.dto.ResponseData;
 import hbi.demo.dto.OmOrderHeaders;
 import hbi.demo.service.IOmOrderHeadersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.validation.BindingResult;
 
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
     @Controller
@@ -35,7 +34,7 @@ import java.util.List;
     @RequestMapping(value = "/hap/om/order/headers/selectExcelData")
     @ResponseBody
     public ResponseData selectExcelData(OmOrderHeaders dto, @RequestParam(defaultValue = DEFAULT_PAGE) int page,
-                              @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int pageSize, HttpServletRequest request) {
+                                        @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int pageSize, HttpServletRequest request) {
         IRequest requestContext = createRequestContext(request);
         return new ResponseData(service.selectExcelData(requestContext,dto,page,pageSize));
     }
@@ -45,9 +44,9 @@ import java.util.List;
     public ResponseData update(@RequestBody List<OmOrderHeaders> dto, BindingResult result, HttpServletRequest request){
         //getValidator().validate(dto, result);
         if (result.hasErrors()) {
-        ResponseData responseData = new ResponseData(false);
-        responseData.setMessage(getErrorMessage(result, request));
-        return responseData;
+            ResponseData responseData = new ResponseData(false);
+            responseData.setMessage(getErrorMessage(result, request));
+            return responseData;
         }
         IRequest requestCtx = createRequestContext(request);
         return new ResponseData(service.myBatchUpdate(requestCtx, dto));
